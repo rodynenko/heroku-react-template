1bde2c61f5fe9f316c727281fc66fc0d08e9e720
Taras Rodynenko <t.rodynenko@gmail.com>
2017-12-22 13:39:24 +0200

    add custom head tags (e.g. title, description, keywords) according to req.path

--- index.js
+++ index.js
@@ -3,6 +3,9 @@ const path = require("path");
 const compression = require("compression");
 const cors = require("cors");
 const app = express();
+const cheerio = require("cheerio");
+const fs = require("fs");
+const pageHead = require("./public/pageHead.json");
 
 app.set('port', process.env.PORT || 3000);
 app.use(compression({ threshold: 1024 }));
@@ -16,11 +19,52 @@ app.get('/*', function (req, res, next) {
 	next();
 });
 
-app.use(express.static(__dirname + '/public'));
 app.use(express.static(__dirname + '/public/assets'));
 
-app.use((req, res) => {
-	res.sendFile(path.join(__dirname + '/public/index.html'));
+app.use((req, res, next) => {
+	if (req.path !== '/') {
+		express.static(__dirname + '/public')(req, res, next);
+		return;
+	}
+
+	next();
+});
+
+app.use((req, res, next) => {
+	const { path } = req;
+	const headTags = pageHead[path];
+	const indexPath = __dirname + '/public/index.html';
+
+	if (headTags) {
+		// add tags to head, and send html
+		fs.readFile(indexPath, { encoding: 'utf-8' }, (err, html) => {
+			if (err) return next(err);
+			const $ = cheerio.load(html);
+			const head = $('head');
+
+			if (headTags.title) {
+				const title = head.find('title');
+
+				if (title.length) {
+					title.text(headTags.title);
+				} else {
+					head.append(`<title>${headTags.title}</title>`);
+				}
+			}
+
+			if (headTags.description) {
+				head.append(`<meta name="description" content="${headTags.description}" />`);
+			}
+
+			if (headTags.keywords) {
+				head.append(`<meta name="keywords" content="${headTags.keywords}" />`);
+			}
+
+			res.send($.html());
+		});
+	} else {
+		res.sendFile(indexPath);
+	}
 });
 
 app.use((err, req, res, next) => {
